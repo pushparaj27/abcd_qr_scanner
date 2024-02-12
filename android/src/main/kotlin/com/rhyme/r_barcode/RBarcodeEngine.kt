0c81b8eb760cea5f7014a5eb38dcc52e5a0002e7
Pushparaj Manickam <pushparaj.manickam@gavstech.com>
2024-02-12 11:22:26 +0530

    remove unwanted comment

--- android/src/main/kotlin/com/rhyme/r_barcode/RBarcodeEngine.kt
+++ android/src/main/kotlin/com/rhyme/r_barcode/RBarcodeEngine.kt
@@ -45,9 +45,7 @@ class RBarcodeEngine {
     private val executor: Executor = Executors.newSingleThreadExecutor()
     private val executorImage: Executor = Executors.newSingleThreadExecutor()
 
-    /**
-     * 初始化所有的编码格式
-     */
+   
     init {
         ALL_FORMATS.add(BarcodeFormat.AZTEC)
         ALL_FORMATS.add(BarcodeFormat.CODABAR)
@@ -75,13 +73,7 @@ class RBarcodeEngine {
         threadHandler.start()
     }
 
-    /**
-     * 初始化解码引擎
-     *
-     * @param isDebug 是否为debug模式
-     * @param formats 要设置的编码集合
-     * @param isReturnImage 是否需要返回图片
-     */
+  
     fun initBarCodeEngine(isDebug: Boolean, formats: List<String>, isReturnImage: Boolean) {
         this.isDebug = isDebug
         this.isReturnImage = isReturnImage
@@ -90,12 +82,7 @@ class RBarcodeEngine {
 //        log(RBarcodeNative.get().stringFromJNI())
     }
 
-    /**
-     * 初始化Flutter事件通信工具
-     *
-     * @param messenger Flutter与原生通信的x信使
-     * @param eventId 事件id
-     */
+  
     fun initEventChannel(messenger: BinaryMessenger, eventId: Long, isDebug: Boolean) {
         if (eventChannel != null) {
             eventChannel!!.dispose()
@@ -105,24 +92,12 @@ class RBarcodeEngine {
         this.isDebug = isDebug
     }
 
-    /**
-     * 设置扫码框
-     *
-     * @param left 左
-     * @param top 上
-     * @param right 右
-     * @param bottom 下
-     */
     fun setCropRect(left: Int, top: Int, right: Int, bottom: Int) {
         cropRect = Rect(left, top, right, bottom)
     }
 
 
-    /**
-     * 获取当前设置的编码格式集合
-     *
-     * @return 编码格式集合,默认为全部编码格式
-     */
+  
     private fun getFormats(): Collection<BarcodeFormat?> {
         return mFormats ?: ALL_FORMATS
     }
@@ -131,11 +106,7 @@ class RBarcodeEngine {
         return getFormats().size == 1 && getFormats().contains(BarcodeFormat.QR_CODE)
     }
 
-    /**
-     * 设置编码格式集合
-     *
-     * @param formats 编码格式集合
-     */
+   
     fun setFormats(formats: List<String>) {
         log("setFormats ${formats.joinToString(separator = "\n")}")
         this.mFormats!!.clear()
@@ -147,16 +118,12 @@ class RBarcodeEngine {
     }
 
 
-    /**
-     * 开始扫码
-     */
+  
     fun startScan() {
         isScan = true
     }
 
-    /**
-     * 关闭扫码
-     */
+    
     fun stopScan() {
         isScan = false
     }
@@ -168,11 +135,7 @@ class RBarcodeEngine {
     private var latestAcquireImageTime: Long = System.currentTimeMillis()
     private var latestScanSuccessTime: Long = System.currentTimeMillis()
 
-    /**
-     * 获取一帧的图片
-     *
-     * @param it 图片读取工具
-     */
+    
     @RequiresApi(Build.VERSION_CODES.KITKAT)
     fun getImage(it: ImageReader): Image? {
         if (System.currentTimeMillis() - latestAcquireImageTime > 150L && System.currentTimeMillis() - latestScanSuccessTime > 150L) {
@@ -196,23 +159,23 @@ class RBarcodeEngine {
             val image = getImage(it) ?: return@execute
             val width = image.width
             val height = image.height
-            val yBuffer = image.planes[0].buffer// 灰图
-            val yLen = yBuffer!!.remaining() // 灰图大小
+            val yBuffer = image.planes[0].buffer
+            val yLen = yBuffer!!.remaining() 
             val yByte = ByteArray(yLen)
-//        var rotateYByte: ByteArray? // y图旋转
-            yBuffer.get(yByte, 0, yLen) // y图 byte获取
+//        var rotateYByte: ByteArray? 
+            yBuffer.get(yByte, 0, yLen) 
             yBuffer.clear()
 
-            val uBuffer = image.planes[1].buffer // 颜色
-            val uLen = uBuffer!!.remaining() // 颜色图大小
-            val uByte = ByteArray(uLen) // 颜色图 byte
-            uBuffer.get(uByte, 0, uLen) // 颜色图 byte获取
+            val uBuffer = image.planes[1].buffer 
+            val uLen = uBuffer!!.remaining() 
+            val uByte = ByteArray(uLen)
+            uBuffer.get(uByte, 0, uLen) 
             uBuffer.clear()
 
-            val vBuffer = image.planes[2].buffer // 饱和度
-            val vLen = vBuffer!!.remaining() //饱和度图大小
-            val vByte = ByteArray(vLen) //饱和度 byte
-            vBuffer.get(vByte, 0, vLen) // 饱和度 byte获取
+            val vBuffer = image.planes[2].buffer 
+            val vLen = vBuffer!!.remaining() 
+            val vByte = ByteArray(vLen) 
+            vBuffer.get(vByte, 0, vLen) 
             vBuffer.clear()
             image.close()
             decodeImageResult(RBarcodeEntity(yByte, uByte, vByte, yLen, uLen, vLen, width, height))
@@ -229,7 +192,7 @@ class RBarcodeEngine {
             log("thread:" + Thread.currentThread() + "  decodeImage first:" + (System.currentTimeMillis() - firstTime) + "ms")
             val secondTime = System.currentTimeMillis()
             if (decodeResult == null && !isOnlyQrCodeFormat()) {
-                // y图旋转
+                
                 val rotateYByte = ByteArray(entity.width * entity.height * 3 / 2)
                 ImageUtils.rotateYUVDegree90(entity.y, rotateYByte, entity.width, entity.height)
                 decodeResult = decodeImage(rotateYByte, entity.width, entity.height)
@@ -244,10 +207,10 @@ class RBarcodeEngine {
             latestScanSuccessTime = System.currentTimeMillis()
 
             if (isReturnImage) {
-                //需要返回原图
+                
                 var resultByte: ByteArray? = null
                 if (isRotate) {
-                    //返回 旋转后的原图
+                    
                     val sourceByte = ByteArray(entity.yLen + entity.uLen + entity.vLen)
                     resultByte = ByteArray(entity.width * entity.height * 3 / 2)
                     System.arraycopy(entity.y, 0, sourceByte, 0, entity.yLen)
@@ -266,7 +229,7 @@ class RBarcodeEngine {
                         entity.height
                     )
                 } else {
-                    // 返回 未旋转的原图
+                    
                     resultByte = ByteArray(entity.yLen + entity.uLen + entity.vLen)
                     System.arraycopy(entity.y, 0, resultByte, 0, entity.yLen)
                     System.arraycopy(entity.u, 0, resultByte, entity.yLen, entity.uLen)
@@ -307,7 +270,7 @@ class RBarcodeEngine {
                 }
 
             } else {
-                //返回不需要图片
+               
                 resultToMapNoImage(
                     decodeResult,
                     entity.width,
@@ -344,14 +307,7 @@ class RBarcodeEngine {
         }
     }
 
-    /**
-     * 解析图片
-     *
-     * @param byte 二维码二值
-     * @param width 二维码宽度
-     * @param height 二维码高度
-     * @return 返回二维码扫描得到的内容
-     */
+   
     private fun decodeImage(byte: ByteArray, width: Int, height: Int): Result? {
         var dataWidth = width
         var dataHeight = height
@@ -395,16 +351,7 @@ class RBarcodeEngine {
         return result
     }
 
-    /**
-     * 返回给Flutter所需要的内容(有图片)
-     *
-     * @param result 二维码扫描的得到的内容
-     * @param image 图片二值
-     * @param width 宽度
-     * @param height 高度
-     * @param isRotate 是否旋转过
-     * @return Flutter 所需要的内容
-     */
+    
     private fun resultToMap(
         result: Result?,
         image: ByteArray,
@@ -420,7 +367,7 @@ class RBarcodeEngine {
         data["imageWidth"] = width;
         data["imageHeight"] = height;
         if (result.resultPoints != null) {
-            //是否返回点位
+            
             val resultPoints: MutableList<Map<String, Any>> = java.util.ArrayList()
             for (point in result.resultPoints) {
                 val pointMap: MutableMap<String, Any> = HashMap()
@@ -440,15 +387,7 @@ class RBarcodeEngine {
         return data
     }
 
-    /**
-     * 返回给Flutter所需要的内容(无图片)
-     *
-     * @param result 二维码扫描的得到的内容
-     * @param width 宽度
-     * @param height 高度
-     * @param isRotate 是否旋转过
-     * @return Flutter 所需要的内容
-     */
+    
     private fun resultToMapNoImage(
         result: Result?,
         width: Int,
